aaabc60e71e6a8efa30cc7a010c4c4e40e7e6f94
YTW <twyun@linkgenesis.co.kr>
2020-01-07 17:09:08 +0900

    Django

---
+++ AIProject/Users/forms.py
@@ -0,0 +1,71 @@
+from django import forms
+from django.contrib.auth.hashers import check_password
+from .models import Users
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(
+        error_messages={
+            'required': '사용자 이름을 입력해주세요.'
+        },
+        max_length=64, label='사용자 이름'
+    )
+    email = forms.EmailField(
+        error_messages={
+            'required': '이메일을 입력해주세요.'
+        },
+        max_length=64, label='이메일'
+    )
+    password = forms.CharField(
+        error_messages={
+            'required': '비밀번호를 입력해주세요.'
+        },
+        widget=forms.PasswordInput, label='비밀번호'
+    )
+    re_password = forms.CharField(
+        error_messages={
+            'required': '비밀번호를 입력해주세요.'
+        },
+        widget=forms.PasswordInput, label='비밀번호 확인'
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get('username')
+        email = cleaned_data.get('email')
+        password = cleaned_data.get('password')
+        re_password = cleaned_data.get('re_password')
+
+        if password and re_password:
+            if password != re_password:
+                self.add_error('password', '비밀번호가 서로 다릅니다.')
+                self.add_error('re_password', '비밀번호가 서로 다릅니다.')
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(
+        error_messages={
+            'required': '이메일을 입력해주세요.'
+        },
+        max_length=64, label='이메일'
+    )
+    password = forms.CharField(
+        error_messages={
+            'required': '비밀번호를 입력해주세요.'
+        },
+        widget=forms.Textarea, label='비밀번호'
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
+        password = cleaned_data.get('password')
+
+        if email and password:
+            try:
+                user = Users.objects.get(email=email)
+            except Users.DoesNotExist:
+                self.add_error('email', '아이디가 없습니다')
+                return
+
+            if not check_password(password, user.password):
+                self.add_error('password', '비밀번호를 틀렸습니다')
---
+++ AIProject/Users/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, redirect
+from django.views.generic import DetailView
+from django.views.generic.edit import FormView
+from django.contrib.auth.hashers import make_password
+from .forms import RegisterForm, LoginForm
+from .models import Users
+
+
+# Create your views here.
+
+def index(request):
+    if request.session.get('user') == None:
+        return redirect('/login')
+    else:
+        return render(request, 'index.html', { 'email': request.session.get('user') })
+
+
+class RegisterView(FormView):
+    template_name = 'register.html'
+    form_class = RegisterForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        user = Users(
+            username= form.data.get('username'),
+            email=form.data.get('email'),
+            password=make_password(form.data.get('password')),
+            level='user'
+        )
+        user.save()
+
+        return super().form_valid(form)
+
+
+class LoginView(FormView):
+    template_name = 'login.html'
+    form_class = LoginForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        self.request.session['user'] = form.data.get('email')
+
+        return super().form_valid(form)
+
+def logout(request):
+    if 'user' in request.session:
+        del(request.session['user'])
+
+    return redirect('/')
